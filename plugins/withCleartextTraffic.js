fc35feaffd6798679017f65c2525f2acc8aa9049
Juan Diego GF <juan.gomez@maker.com.mx>
2026-08-07 15:26:19 -0600

    Config plugin para habilitar trafico HTTP (cleartext) en Android

---
+++ plugins/withCleartextTraffic.js
@@ -0,0 +1,12 @@
+const { withAndroidManifest } = require('@expo/config-plugins');
+
+module.exports = function withCleartextTraffic(config) {
+  return withAndroidManifest(config, (config) => {
+    const app = config.modResults.manifest.application?.[0];
+    if (app) {
+      app.$ = app.$ ?? {};
+      app.$['android:usesCleartextTraffic'] = 'true';
+    }
+    return config;
+  });
+};
